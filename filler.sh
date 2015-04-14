22333d82ee374636ea7ac212f6a3d4a39f7679cf
José Tomás Tocino <theom3ga@gmail.com>
2015-04-14 08:59:13 +0200

    First commit

---
+++ filler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+currentDay=$(date +"%d")
+
+function modifyAndCommit {
+    echo $RANDOM > $1
+    git commit $1 -m "Modified $1"
+}
+
+if (( $currentDay % 2 == 0))
+then
+    modifyAndCommit a
+    modifyAndCommit b
+    modifyAndCommit c
+else
+    modifyAndCommit a
+fi
+
+git push master
